240f8d234a831f8cd40dce9ad4e5c0fe0e9dc2fa
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-16 16:19:53 -0600

    finished input functions and player turn

--- src/Player.java
+++ src/Player.java
@@ -5,7 +5,7 @@
 * CLASS:             CS200 – Object Oriented Programming              *
 * INSTRUCTOR:        Dean Zeller                                      *
 * TERM:              Spring 2021                                      *
-* SUBMISSION DATE:   4/9/2021		                                  *
+* SUBMISSION DATE:   4/16/2021		                                  *
 *                                                                     *
 * DESCRIPTION:                                                        *
 * The following is an OOP definition for a Player in a game of        *
@@ -25,6 +25,7 @@
 **********************************************************************/
 
 import java.text.MessageFormat;
+import java.util.Scanner;
 
 public class Player {
 	
@@ -33,6 +34,7 @@ public class Player {
 	private int height;
 	private char[][] playerBoard;
 	private char[][] opponentBoard;
+	private Scanner input;
 	
 	public Player() {
 		this.name = "Player 1";
@@ -46,6 +48,7 @@ public class Player {
 				this.opponentBoard[i][j] = '.';
 			}
 		}
+		this.input = new Scanner(System.in);
 	}
 	
 	public void Display_Player_Board() {
@@ -101,6 +104,101 @@ public class Player {
 		}
 	}
 	
+	public void Change_Board(int firstCoordinate, int secondCoordinate) {
+		this.playerBoard[firstCoordinate - 1][secondCoordinate - 1] = '@';
+	}
 	
+	public boolean Validate_First_Coordinate(String firstCoordinate) {
+		boolean validInput = false;
+		
+		if (firstCoordinate.toUpperCase().equals("A") ||
+				firstCoordinate.toUpperCase().equals("B") ||
+				firstCoordinate.toUpperCase().equals("C") ||
+				firstCoordinate.toUpperCase().equals("D") ||
+				firstCoordinate.toUpperCase().equals("E") ||
+				firstCoordinate.toUpperCase().equals("F") ||
+				firstCoordinate.toUpperCase().equals("G") ||
+				firstCoordinate.toUpperCase().equals("H") ||
+				firstCoordinate.toUpperCase().equals("I") ||
+				firstCoordinate.toUpperCase().equals("J")) {
+			validInput = true;
+		}
+		
+		return validInput;
+	}
+	
+	public boolean Validate_Second_Coordinate(String secondCoordinate) {
+		boolean validInput = true;
+		int convertedInput = 0;
+		
+		try {
+			convertedInput = Integer.parseInt(secondCoordinate);
+		} catch (Exception e) {
+			validInput = false;
+		}
+		
+		if (convertedInput < 1 || convertedInput > 10) {
+			validInput = false;
+		}
+		
+		return validInput;
+	}
+	
+	public int Convert_Input(String input) {
+		if (input.toUpperCase().equals("A")) { return 1; }
+		else if (input.toUpperCase().equals("B")) { return 2; }
+		else if (input.toUpperCase().equals("C")) { return 3; }
+		else if (input.toUpperCase().equals("D")) { return 4; }
+		else if (input.toUpperCase().equals("E")) { return 5; }
+		else if (input.toUpperCase().equals("F")) { return 6; }
+		else if (input.toUpperCase().equals("G")) { return 7; }
+		else if (input.toUpperCase().equals("H")) { return 8; }
+		else if (input.toUpperCase().equals("I")) { return 9; }
+		else if (input.toUpperCase().equals("J")) { return 10; }
+		else { return 0; }
+	}
+	
+	public void Player_Turn() {
+		System.out.println("\n" + this.name + "'s Turn.");
+		this.Display_Player_Board();
+		
+		boolean validInput = false;
+		while (!validInput) {
+			System.out.print("\nCoordinates: ");
+			String coordinates = this.input.nextLine();
+			String[] fullCoordinates = coordinates.split("");
+			
+			if (fullCoordinates.length <= 3) {
+				String firstCoordinatePart = fullCoordinates[0];
+				String secondCoordinatePart = "";
+				
+				if (fullCoordinates.length == 2) { secondCoordinatePart = fullCoordinates[1]; }
+				else { secondCoordinatePart = fullCoordinates[1] + fullCoordinates[2]; }
+				
+				if (this.Validate_First_Coordinate(firstCoordinatePart) == true && this.Validate_Second_Coordinate(secondCoordinatePart) == true) {
+					int firstCoordinate = this.Convert_Input(firstCoordinatePart);
+					int secondCoordinate = Integer.parseInt(secondCoordinatePart);
+					this.Change_Board(secondCoordinate, firstCoordinate); //Had to flip the two coordinates based on how the 2d array works
+					validInput = true;
+				} else {
+					System.out.println("Please enter valid coordinates.");
+				}
+			} else {
+				System.out.println("Please enter the coordinates that you'd like to fire on.");
+			}
+		}
+		
+	}
+	
+	public void Opponent_Turn() {
+		
+	}
+	
+	public void Run_Until_Close() {
+		boolean isRunning = true;
+		while (isRunning) {
+			this.Player_Turn();
+		}
+	}
 
 }
