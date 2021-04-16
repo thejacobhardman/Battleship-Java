bc4da754c403543b20e400ee6880d0095a731359
Jacob Hardman <jacobhardmanbusiness@gmail.com>
2021-04-16 14:53:09 -0600

    create base files and add block documentation

---
+++ src/Player.java
@@ -0,0 +1,29 @@
+/**********************************************************************
+*                  Assignment 9 -- Battleship                         *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   4/9/2021		                                  *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is an OOP definition for a Player in a game of        *
+* Battleship.                                                         *
+*                                                                     *
+* ATTRIBUTES:                                                         *
+* 	name -- The name of the player, stored as a String.               *
+* 	width -- The width of the player's gameboard, stored as an int.	  *
+*   height -- The height of the player's gameboard, stored as an int. *
+*   playerBoard -- The 2d array that store's the gameboard's values.  *
+*   opponentBoard -- The 2d array that store's the opponent's         *
+*   gameboard's value.                                                *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program is copyright (c)2021 Dean Zeller and Jacob Hardman.    *
+*                                                                     *
+**********************************************************************/
+
+public class Player {
+
+}
---
+++ src/PlayerTester.java
@@ -0,0 +1,25 @@
+/**********************************************************************
+*                  Assignment 9 -- Battleship                         *
+*                                                                     *
+* PROGRAMMER:        Jacob Hardman - hard7293@bears.unco.edu          *
+* CLASS:             CS200 – Object Oriented Programming              *
+* INSTRUCTOR:        Dean Zeller                                      *
+* TERM:              Spring 2021                                      *
+* SUBMISSION DATE:   4/16/2021                            		      *
+*                                                                     *
+* DESCRIPTION:                                                        *
+* The following is a tester for the Player object.                    *
+*                                                                     *
+* COPYRIGHT:                                                          *
+* This program is copyright (c)2021 Dean Zeller and Jacob Hardman.    *
+*                                                                     *
+**********************************************************************/
+
+public class PlayerTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
